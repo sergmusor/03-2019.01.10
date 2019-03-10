2af41f9cf251f439cf5a9b06ea64930cef87f01d
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-03-10 08:23:00 +0300

    Remove files for review

--- test2.cpp
+++
@@ -1,8 +0,0 @@
-#include "lib.hpp"
-
-#include <cstdlib>
-#include <iostream>
-
-int main() {
-	return std::cout << "Version: " << ver() << std::endl && std::cout << "Message: " << msg() << std::endl ? EXIT_SUCCESS : EXIT_FAILURE;
-}
