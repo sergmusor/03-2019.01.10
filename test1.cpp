66889505d4615f1b77ca3221f89fcc55c3d672ff
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-03-10 08:24:28 +0300

    Remove files for review

--- test1.cpp
+++
@@ -1,8 +0,0 @@
-#include "lib.hpp"
-
-#include <cstdlib>
-#include <iostream>
-
-int main() {
-	return ver() == std::to_string(major()) + '.' + std::to_string(minor()) + '.' + std::to_string(patch()) ? EXIT_SUCCESS : EXIT_FAILURE;
-}
