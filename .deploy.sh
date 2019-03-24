ea21370893990e7e0a482e29d16018273145a0c0
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-03-24 18:17:30 +0300

    .deploy.sh: move publish params to a distinct variable

--- .deploy.sh
+++ .deploy.sh
@@ -5,8 +5,9 @@ project='03-2019.01.10'
 name='ipfilter'
 version="0.0.$1"
 user='sergmusor'
+params=';deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1'
 
 for component in prog lib; do
 	package="${name}-${component}_${version}_amd64.deb"
-	curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
+	curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package$params"
 done
