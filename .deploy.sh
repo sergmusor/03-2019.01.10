cad25363112aef33a73c546931e56e9c23faa41e
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-03-24 18:13:54 +0300

    .deploy.sh: fix a typo putting a quote at its palce

--- .deploy.sh
+++ .deploy.sh
@@ -8,5 +8,5 @@ user='sergmusor'
 
 for component in prog lib; do
 	package="${name}-${component}_${version}_amd64.deb"
-	curl -T "$package" -u sergmusor:"$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
+	curl -T "$package" -u "${user}:$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
 done
