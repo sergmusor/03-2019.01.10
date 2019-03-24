3a9117bd84ac0371b5ed4d77821703c08f031934
sergmusor <46078083+sergmusor@users.noreply.github.com>
2019-03-24 07:56:36 +0300

    deploy.sh: parametrize the script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #! /bin/sh -e
 
-for suffix in prog lib; do
-	curl -T "ipfilter-${suffix}_0.0.${1}_amd64.deb" -u sergmusor:"$2" "https://api.bintray.com/content/sergmusor/03-2019.01.10/ipfilter/0.0.${1}/pool/main/i/ipfilter/ipfilter-${suffix}_0.0.${1}_amd64.deb;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
+name='ipfilter'
+version="0.0.$1"
+baseurl='https://api.bintray.com/content'
+user='sergmusor'
+project='03-2019.01.10'
+
+for component in prog lib; do
+	package="${name}-${component}_${version}_amd64.deb"
+	curl -T "$package" -u sergmusor:"$2" "$baseurl/$user/$project/$name/$version/pool/main/${name::1}/$name/$package;deb_distribution=xenial;deb_component=main;deb_architecture=amd64;publish=1"
 done
